4429cd946f8d886290206d1edd669c433e1aef00
Maryna <89296066105marina@gmal.com>
2024-11-19 11:13:25 +0100

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,15 @@
+const btn = document.querySelector(".change-color");
+
+const body = document.querySelector("body");
+
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
