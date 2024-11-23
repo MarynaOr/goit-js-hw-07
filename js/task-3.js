79b41d85ad90617863d0155a6bc1d6448c2bb423
Maryna <89296066105marina@gmal.com>
2024-11-23 12:08:30 +0100

    task-3_style

--- js/task-3.js
+++ js/task-3.js
@@ -10,3 +10,31 @@ function inpFoo(event) {
   }
   title.textContent = input.value;
 }
+
+
+
+//
+input.style.width = '360px';
+input.style.height = '40px';
+input.style.color = '#2e2f42';
+
+
+const titleStyle = document.querySelector('h1')
+
+
+
+
+titleStyle.style.fontWeight = '600';
+titleStyle.style.fontSize = '24px';
+titleStyle.style.lineHeight = '1.3';
+titleStyle.style.letterSpacing = '0.04em';
+titleStyle.style.color = '#2e2f42';
+
+
+
+
+
+
+
+
+
