825ee6cd9fd4ab78253c3f6b525a0d11ae95ef5e
Maryna <89296066105marina@gmal.com>
2024-11-23 14:53:43 +0100

    task-5_style

--- js/task-5.js
+++ js/task-5.js
@@ -15,3 +15,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+//
+const paragraf = document.querySelector(".widget p");
+
+paragraf.style.fontWeight = "400";
+paragraf.style.fontSize = "16px";
+paragraf.style.lineHeight = "1.5";
+
+paragraf.style.color = "#2e2f42";
+paragraf.style.letterSpacing = "0.04em";
+
+btn.style.marginTop = "16px";
+btn.style.borderRadius = "8px";
+btn.style.padding = "8px 16px";
+btn.style.width = "148px";
+btn.style.height = "40px";
+btn.style.backgroundColor = "#4e75ff";
+btn.style.border = "none";
